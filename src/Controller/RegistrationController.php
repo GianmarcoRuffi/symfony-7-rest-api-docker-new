0d34b93da8f4b73e026850e4d85be53f001356fd
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2024-04-05 18:02:42 +0200

    staging-4: code cleanup

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -41,21 +41,8 @@ class RegistrationController extends AbstractController
                 )
             );
 
-
-            // generate a signed url and email it to the user
-            // $this->emailVerifier->sendEmailConfirmation(
-            //     'app_verify_email',
-            //     $user,
-            //     (new TemplatedEmail())
-            //         ->from(new Address('noreply@test-mail.com', 'Giammy Mail Bot'))
-            //         ->to($user->getEmail())
-            //         ->subject('Please Confirm your Email')
-            //         ->htmlTemplate('registration/confirmation_email.html.twig')
-            // );
             $entityManager->persist($user);
             $entityManager->flush();
-
-            // do anything else you need here, like send an email
 
             return $security->login($user, 'form_login', 'main');
         }
@@ -64,25 +51,4 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form,
         ]);
     }
-
-    // #[Route('/verify/email', name: 'app_verify_email')]
-    // public function verifyUserEmail(Request $request): Response
-    // {
-    //     $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-    //     // validate email confirmation link, sets User::isVerified=true and persists
-    //     try {
-    //         $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
-    //         echo "Hello, world!";
-    //     } catch (VerifyEmailExceptionInterface $exception) {
-    //         $this->addFlash('verify_email_error', $exception->getReason());
-
-    //         return $this->redirectToRoute('app_register');
-    //     }
-
-    //     // @TODO Change the redirect on success and handle or remove the flash message in your templates
-    //     $this->addFlash('success', 'Your email address has been verified.');
-
-    //     return $this->redirectToRoute('/');
-    // }
 }
